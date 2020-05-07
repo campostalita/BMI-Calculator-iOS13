e6c9bf081c99ae6eb0db837ad6c07e1484d010e7
Talita Campos <campos.talita@hotmail.com>
2020-05-06 20:26:10 -0400

    Calculadora Ok

---
+++ BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by user167993 on 5/6/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
